274aeafb345a1fd1a7f50b59a328ca9f10f556c5
Density006 <nate@studio.digital>
2025-10-30 15:01:30 +0800

    Update impersonate.ts

--- pages/api/impersonate.ts
+++ pages/api/impersonate.ts
@@ -2,36 +2,7 @@
 import { withIronSessionApiRoute } from 'iron-session/next'
 import { sessionOptions, SessionData } from 'lib/session'
 import { NextApiRequest, NextApiResponse } from 'next'
-
-// --- We need the user list here too ---
-const DEFAULT_REDIRECT_URL = 'https://sites.google.com/studio.digital/multitool/Hw2'
-const DENSITY_REDIRECT_URL = 'https://sites.google.com/studio.digital/multitool/Dense'
-const ETHAN_REDIRECT_URL = 'https://ethans-page.com'
-const DISALLOWED_REDIRECT_URL = 'https://sites.google.com/studio.digital/hmmm/home'
-const GITHUB_REDIRECT_URL = 'https://github.com/Density006/Density006password'
-const TEDDBLUE = 'https://gamebois-inky.vercel.app/'
-
-interface UserCredentials {
-  pwd: string
-  redirect?: string
-}
-
-// We only need the users and their redirects here
-const validCredentials = new Map<string, UserCredentials>([
-  ['Density006', { pwd: 'dense', redirect: DENSITY_REDIRECT_URL }],
-  ['Ethan', { pwd: 'ethan' }],
-  ['4dmin', { pwd: 'NATE', redirect: '/admin' }],
-  ['Jimmy', { pwd: 'germanleader' }],
-  ['Test', { pwd: 'Nate' }],
-  ['Jamesmann', { pwd: 'wisehelp88' }],
-  ['Duke', { pwd: 'danby', redirect: DISALLOWED_REDIRECT_URL }],
-  ['add user', { pwd: '', redirect: GITHUB_REDIRECT_URL }],
-  ['new1', { pwd: 'coldnewt29', redirect: TEDDBLUE }],
-  ['JamesH', { pwd: '  ' }],
-  ['Keaton', { pwd: 'keaton' }],
-  ['shawto29', { pwd: 'darkram60' }],
-])
-// --- End of user list ---
+import { validCredentials, DEFAULT_REDIRECT_URL } from 'lib/users' // <-- Import from shared file
 
 export default withIronSessionApiRoute(impersonateRoute, sessionOptions)
 
