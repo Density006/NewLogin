e7530fa2c16db8cb1f642e588d4148c95ee5f000
Density006 <nate@studio.digital>
2025-10-30 21:06:48 +0800

    Update session.ts

--- lib/session.ts
+++ lib/session.ts
@@ -12,7 +12,7 @@ export type SessionData = {
 export const sessionOptions: IronSessionOptions = {
   // IMPORTANT: You MUST set this password in your Vercel project's Environment Variables
   // Go to your project on Vercel -> Settings -> Environment Variables
-  // Create a new variable named 'SECRET_COOKIE_PASSWORD'
+  // Create a new variable named 'SECRET_COOKIE_PASSWORD
   // Use a strong, random password (e.g., from https://1password.com/password-generator/)
   password: process.env.SECRET_COOKIE_PASSWORD as string,
   cookieName: 't8wFg4W2wbg0oRnoeGQqstczmRU2NsW1',
